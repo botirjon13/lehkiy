8684b0b79bf28e178cad6d7f9aae2e907abfb96c
botirjon13 <tomama-uz@mail.ru>
2025-10-03 17:28:54 +0500

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")  # Токен телеграм-бота из переменных окружения
-DATABASE_URL = os.getenv("DATABASE_URL")  # PostgreSQL URL (Railway и т.п.)
-
-ADMIN_ID = int(os.getenv("ADMIN_ID", 0))  # Telegram ID администратора для отчетов
