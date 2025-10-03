0b20d8a51f463fa25b235b0f5f8d4fb00dd22b64
botirjon13 <tomama-uz@mail.ru>
2025-10-03 11:28:04 +0500

    Update db.py

--- db.py
+++ db.py
@@ -1,43 +1,40 @@
-import asyncpg
 import os
+import asyncpg
 
-DB_URL = os.getenv("DATABASE_URL")  # Railway автоматически подставит DATABASE_URL
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 async def connect_db():
-    return await asyncpg.connect(DB_URL)
+    return await asyncpg.connect(DATABASE_URL)
 
 async def create_tables(conn):
     # Таблица клиентов
     await conn.execute("""
-    CREATE TABLE IF NOT EXISTS clients (
-        id BIGINT PRIMARY KEY,
-        username TEXT
-    );
+        CREATE TABLE IF NOT EXISTS clients (
+            id BIGINT PRIMARY KEY,
+            username TEXT
+        );
     """)
-
     # Таблица товаров
     await conn.execute("""
-    CREATE TABLE IF NOT EXISTS products (
-        id SERIAL PRIMARY KEY,
-        name TEXT NOT NULL,
-        price NUMERIC(10, 2) NOT NULL
-    );
+        CREATE TABLE IF NOT EXISTS products (
+            id SERIAL PRIMARY KEY,
+            name TEXT NOT NULL,
+            price NUMERIC(10,2) NOT NULL
+        );
     """)
-
     # Таблица продаж
     await conn.execute("""
-    CREATE TABLE IF NOT EXISTS sales (
-        id SERIAL PRIMARY KEY,
-        client_id BIGINT REFERENCES clients(id),
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
+        CREATE TABLE IF NOT EXISTS sales (
+            id SERIAL PRIMARY KEY,
+            client_id BIGINT REFERENCES clients(id),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
     """)
-
-    # Таблица связки товаров и продаж
+    # Таблица связей продаж–товары
     await conn.execute("""
-    CREATE TABLE IF NOT EXISTS sales_products (
-        id SERIAL PRIMARY KEY,
-        sale_id INTEGER REFERENCES sales(id),
-        product_id INTEGER REFERENCES products(id)
-    );
+        CREATE TABLE IF NOT EXISTS sales_products (
+            sale_id INT REFERENCES sales(id),
+            product_id INT REFERENCES products(id),
+            PRIMARY KEY (sale_id, product_id)
+        );
     """)
