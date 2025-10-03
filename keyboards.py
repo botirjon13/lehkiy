116eed01c17d03b8c66952c372ddc9662dce85ab
botirjon13 <tomama-uz@mail.ru>
2025-10-03 14:26:17 +0500

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -3,10 +3,12 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 def get_start_keyboard():
     builder = ReplyKeyboardBuilder()
-    builder.add(KeyboardButton("Ro'yxatdan o'tish", request_contact=True))
+    # Здесь используем только именованные аргументы
+    button = KeyboardButton(text="Ro'yxatdan o'tish", request_contact=True)
+    builder.add(button)
     return builder.as_markup(resize_keyboard=True)
 
 def get_confirm_sale_keyboard():
     kb = InlineKeyboardMarkup()
-    kb.add(InlineKeyboardButton("Tasdiqlash", callback_data="confirm_sale"))
+    kb.add(InlineKeyboardButton(text="Tasdiqlash", callback_data="confirm_sale"))
     return kb
